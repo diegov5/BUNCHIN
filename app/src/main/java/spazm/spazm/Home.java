0625e869d44441cd852862e4d63d3c3ff28a8e2d
Diego <diegov0824@hotmail.com>
2018-04-10 21:11:36 -0700

    Uploading rest of the files

---
+++ app/src/main/java/spazm/spazm/Home.java
@@ -0,0 +1,17 @@
+package spazm.spazm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Diego Valdez Local on 4/5/2018.
+ */
+
+public class Home extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}

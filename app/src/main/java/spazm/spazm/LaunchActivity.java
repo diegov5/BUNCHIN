3cb491324675af2d320dc63bf3369a9c4e919d77
Diego <diegov0824@hotmail.com>
2018-04-10 21:56:07 -0700

    Updated login button to launch home

--- app/src/main/java/spazm/spazm/LaunchActivity.java
+++ app/src/main/java/spazm/spazm/LaunchActivity.java
@@ -1,7 +1,12 @@
 package spazm.spazm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,6 +14,16 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        Button login = (Button) findViewById(R.id.LOGIN);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(view.getContext(), Home.class);
+                view.getContext().startActivity(Intent);}
+        });
+
     }
+
     // test
 }

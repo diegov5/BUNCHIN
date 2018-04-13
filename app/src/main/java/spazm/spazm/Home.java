e6701636b3e979aa03ea153e3d844a1cb0377cad
Diego <diegov0824@hotmail.com>
2018-04-12 20:35:56 -0700

    Worked on home, still attempting to get initial grid working properly

--- app/src/main/java/spazm/spazm/Home.java
+++ app/src/main/java/spazm/spazm/Home.java
@@ -2,6 +2,10 @@ package spazm.spazm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by Diego Valdez Local on 4/5/2018.
@@ -13,5 +17,16 @@ public class Home extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        GridView gridview = findViewById(R.id.hotGridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        /* gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Home.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }); */
     }
 }
---
+++ app/src/main/java/spazm/spazm/ImageAdapter.java
@@ -0,0 +1,55 @@
+package spazm.spazm;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+/**
+ * Created by Diego Valdez Local on 4/12/2018.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(10, 10));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(2, 2, 2, 2);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+}

f2d1b476ff5bdc90e01b8c0fc5587da649e7c4d0
dihle2020 <38272799+dihle2020@users.noreply.github.com>
2018-05-02 15:06:27 -0700

    Edited maxScore function, Created signup activity and add them to static ArrayList in User class

--- app/src/main/java/spazm/spazm/ImageGalleryActivity.java
+++ app/src/main/java/spazm/spazm/ImageGalleryActivity.java
@@ -1,94 +1,106 @@
 package spazm.spazm;
-/*
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.database.Cursor;
-import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import java.io.IOException;
+import java.util.List;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class ImageGalleryActivity extends AppCompatActivity implements EasyPermissions {
-
-
-    private static int RESULT_LOAD_IMAGE = 1;
-
+public class ImageGalleryActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+    private static final String TAG = ImageGalleryActivity.class.getSimpleName();
+    private static final int TAKE_PICTURE = 1;
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private Bitmap bitmap;
+    private Uri capturedImageUri;
+    private String selectedImagePath;
+    private ImageView photoImage;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseprofilepic);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        photoImage = (ImageView)findViewById(R.id.camera_image);
+        Button cameraButton =  (Button)findViewById(R.id.camera_button);
+        if(cameraButton != null)
+        {
+            cameraButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, TAKE_PICTURE);
+                }
+            });
+        }
 
-        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
-        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
-            }
-        });
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, MainActivity.this);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, ImageGalleryActivity.this);
     }
-
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        if(EasyPermissions.hasPermissions(this, galleryPermissions))
-        {
-            getProfilePic(requestCode,resultCode,data);
-        }else
-        {
-            EasyPermissions.requestPermissions(this, "Access for storage",
-                101, galleryPermissions);
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been granted");
+        if(capturedImageUri != null){
+            selectedImagePath = getRealPathFromURIPath(capturedImageUri, ImageGalleryActivity.this);
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), capturedImageUri);
+                Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, 500, 400, true);
+                photoImage.setImageBitmap(resizedBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-
-
     }
-
-    private void getProfilePic(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been denied");
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == 1) {
+            String result = data.toURI();
+            capturedImageUri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                selectedImagePath = getRealPathFromURIPath(capturedImageUri, ImageGalleryActivity.this);
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), capturedImageUri);
+                    Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, 500, 400, true);
+                    photoImage.setImageBitmap(resizedBitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.rationale_camera), CAMERA_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
             cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            ImageView imageView = (ImageView) findViewById(R.id.PROFILEPIC);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
         }
     }
-}*/
-
+}
+/*
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
@@ -190,7 +202,7 @@ public class ImageGalleryActivity extends AppCompatActivity {
             return cursor.getString(idx);
         }
     }
-}
+}*/
 
 
 

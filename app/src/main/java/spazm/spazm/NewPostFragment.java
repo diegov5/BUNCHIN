3cbe6fefb60183c756d08a814ab962a69f1d74c9
Diego <diegov0824@hotmail.com>
2018-05-04 16:20:10 -0700

    Take Picture works as intended and uploads picture like choose picture

--- app/src/main/java/spazm/spazm/NewPostFragment.java
+++ app/src/main/java/spazm/spazm/NewPostFragment.java
@@ -1,15 +1,21 @@
 package spazm.spazm;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,21 +23,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
+import static spazm.spazm.CameraActivity.REQUEST_IMAGE_CAPTURE;
 
 public class NewPostFragment extends Fragment {
 
     public static final int PICK_IMAGE = 1;
-    private static final int RESULT_LOAD_IMAGE = 1;
-    public Button takePic;
-    private Button post;
+    public static final int REQUEST_IMAGE_CAPTURE = 2;
     public ImageView imageView;
     private EditText title;
     private EditText numLikes;
+    private String mCurrentPhotoPath;
 
     public NewPostFragment() {}
 
@@ -41,17 +50,28 @@ public class NewPostFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         getActivity().setTitle("New Post");
         final View rootView = inflater.inflate(R.layout.fragment_newpost, container, false);
+        imageView = rootView.findViewById(R.id.NEW_POST);
+        Button post = rootView.findViewById(R.id.POST);
 
-        takePic = (Button) rootView.findViewById(R.id.takePic);
+        Button takePic = rootView.findViewById(R.id.takePic);
         takePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent Intent = new Intent(view.getContext(), CameraActivity.class);
-                view.getContext().startActivity(Intent);
+                /*Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (getActivity().checkSelfPermission(Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                    REQUEST_IMAGE_CAPTURE);
+                        }
+                    }*/
+                    dispatchTakePictureIntent();
+                //}
             }
         });
 
-        Button choosePic = (Button) rootView.findViewById(R.id.choosePic);
+        Button choosePic = rootView.findViewById(R.id.choosePic);
         choosePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,18 +82,13 @@ public class NewPostFragment extends Fragment {
             }
         });
 
-
-        imageView = (ImageView) rootView.findViewById(R.id.NEW_POST);
-
-        post = (Button) rootView.findViewById(R.id.POST);
         post.setOnClickListener(new View.OnClickListener() {
-
             //Drawable myDrawable = getResources().getDrawable(R.drawable.NEW_POST);
             @Override
             public void onClick(View view) {
                 //Integer ID = imageView;
-                title = (EditText) rootView.findViewById(R.id.LABEL);
-                numLikes = (EditText) rootView.findViewById(R.id.NUMLIKES);
+                title = rootView.findViewById(R.id.LABEL);
+                numLikes = rootView.findViewById(R.id.NUMLIKES);
                 //TopPost newPost = new TopPost(imageView, title.getText().toString(), numLikes.getText().toString());
 
             }
@@ -83,11 +98,82 @@ public class NewPostFragment extends Fragment {
 
     }
 
+    private void dispatchTakePictureIntent() {
+        /*Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getContext(),"com.example.android.fileprovider",photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(Intent.createChooser(takePictureIntent, "Take Picture"), REQUEST_IMAGE_CAPTURE);
+            }
+        }*/
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == 1 && null != data) {
-            decodeUri(data.getData());
-        }
+            if (resultCode == REQUEST_IMAGE_CAPTURE || requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                BitmapFactory.Options o = new BitmapFactory.Options();
+
+                o.inJustDecodeBounds = true;
+
+                // the new size we want to scale to
+                final int REQUIRED_SIZE = 1024;
+
+                // Find the correct scale value. It should be the power of 2.
+                int width_tmp = o.outWidth, height_tmp = o.outHeight;
+                int scale = 1;
+                while (true) {
+                    if (width_tmp < REQUIRED_SIZE && height_tmp < REQUIRED_SIZE) {
+                        break;
+                    }
+                    width_tmp /= 2;
+                    height_tmp /= 2;
+                    scale *= 2;
+                }
+
+                // decode with inSampleSize
+
+                BitmapFactory.Options o2 = new BitmapFactory.Options();
+                o2.inSampleSize = scale;
+                Bitmap croppedBitmap = Bitmap.createScaledBitmap(imageBitmap, 550, 550, true);
+
+                imageView.setImageBitmap(croppedBitmap);
+            }
+            else{
+                decodeUri(data.getData());
+            }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     public void decodeUri(Uri uri) {
@@ -120,9 +206,8 @@ public class NewPostFragment extends Fragment {
             BitmapFactory.Options o2 = new BitmapFactory.Options();
             o2.inSampleSize = scale;
             Bitmap bitmap = BitmapFactory.decodeFileDescriptor(imageSource, null, o2);
-            Bitmap croppedBitmap = bitmap.createScaledBitmap(bitmap, 550, 550, true);
+            Bitmap croppedBitmap = Bitmap.createScaledBitmap(bitmap, 550, 550, true);
             imageView.setImageBitmap(croppedBitmap);
-
         } catch (FileNotFoundException e) {
             // handle errors
         } finally {
@@ -134,6 +219,4 @@ public class NewPostFragment extends Fragment {
                 }
         }
     }
-
-
 }
